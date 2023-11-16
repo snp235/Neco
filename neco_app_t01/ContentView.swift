8a6e38a9751bfaa707801a07340d10ed592e97bd
snp235 <anabeatrizleite235@gmail.com>
2023-11-16 19:00:16 -0300

    Initial Commit

---
+++ neco_app_t01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  neco_app_t01
+//
+//  Created by Ana Beatriz Araújo Leite on 16/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ neco_app_t01/neco_app_t01App.swift
@@ -0,0 +1,17 @@
+//
+//  neco_app_t01App.swift
+//  neco_app_t01
+//
+//  Created by Ana Beatriz Araújo Leite on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct neco_app_t01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
